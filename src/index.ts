ecd9690b2fcac82268e4551228ccb748a61ec881
faisaltheprofessor <faisalfazily@gmail.com>
2024-05-23 20:42:12 +0200

    timestamp fail response

--- src/index.ts
+++ src/index.ts
@@ -94,7 +94,9 @@ app.post('/sensors/data', (req: Request, res: Response) => {
 
         if (!timestamp || !/^\d{4}-\d{2}-\d{2} (?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d$/.test(timestamp)) {
             console.log(timestamp)
-            errors.push(!timestamp ? 'timestamp is missing' : 'timestamp must be in the format: yyyy-mm-dd HH:mm:ss. Valid time range is from 00:00:00 to 23:59:59');
+            errors.push(!timestamp ? 'timestamp is missing' : 'Timestamp must be valid and in the format: yyyy-mm-dd HH:mm:ss');
+            return res.status(500).json({ success: false, error: 'Invalid timestamp' })
+
         }
 
     if (errors.length > 0) {
